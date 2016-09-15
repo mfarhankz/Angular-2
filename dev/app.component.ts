14297003d7914a29c0d86901dd6b7f2114cebd27
mfarhankz <m.farhankz@gmail.com>
2016-09-16 04:59:46 +0500

    learn create component and some other thing and add bootstrap

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,27 @@
 import {Component} from 'angular2/core';
+import {FooterComponent} from './footer.component'
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+       <h1>Hello World!</h1>
+       <p>
+        I am {{name}} <span [class.like]="likeMe.value === 'yes'">do you like me</span>
+      </p>
+      <br>
+      <br>
+      <input type="text" #likeMe (keyup)="0">
+      <br>
+      <br>
+      <button class="btn btn-primary" [disabled]="likeMe.value !== 'yes'">Only Enable if answer is yes</button>
+      <br>
+      <br>
+       <footer></footer>
     `,
+    directives: [FooterComponent],
+    styleUrls: ['src/css/app.css']
+
 })
 export class AppComponent {
-
+  name = 'Farhan';
 }
---
+++ dev/footer.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'footer',
+  template: 'footer'
+})
+
+export class FooterComponent {
+
+}
+
